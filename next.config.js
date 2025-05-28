2dfdcc9207289d84fb7f45efdddd0ac6e83be895
Jaseemuddin Naseem <jaseemuddinn@gmail.com>
2025-05-29 00:11:44 +0530

    config file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['your-image-domain.com'], // Replace with your image domain if needed
+    domains: [''], 
   },
   env: {
-    DATABASE_URL: process.env.DATABASE_URL, // Add your database URL here
-    LOYALTY_PROGRAM_SETTINGS: process.env.LOYALTY_PROGRAM_SETTINGS, // Add any other environment variables
+    DATABASE_URL: process.env.DATABASE_URL, 
+    LOYALTY_PROGRAM_SETTINGS: process.env.LOYALTY_PROGRAM_SETTINGS, 
   },
 };
